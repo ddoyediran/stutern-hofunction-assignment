1062c6cfdb2663b7b6b3fa4e9864505ead69b327
Damilare Oyediran <43407276+ddoyediran@users.noreply.github.com>
2022-10-29 15:11:20 +0200

    Update higherOrderFunction.js

--- higherOrderFunction.js
+++ higherOrderFunction.js
@@ -131,3 +131,25 @@ function findIndex(anArray, callback){
 console.log(findIndex([8,11,4,27], function(val){return val >= 10})); // 1
 console.log(findIndex([8,11,4,27], function(val){return val === 5})); // undefined
 
+
+
+// Question 9
+/**
+  Write a function called specialMultiply which accepts two parameters. If the function is passed both parameters, it should return 
+  the product of the two. If the function is only passed one parameter - it should return a function which can later be passed another 
+  parameter to return the product. You will have to use closure and arguments to solve this.
+*/
+
+function specialMultiply(num1, num2){
+  if(arguments.length === 2){
+    return num1 * num2;
+  }
+  return function inner(num2){
+    return num1 * num2;
+  }
+}
+
+
+console.log(specialMultiply(3)(4)) // 8
+console.log(specialMultiply(3, 4)) // 12
+console.log(specialMultiply(3)) // function inner(num2){ return num1 * num2; }

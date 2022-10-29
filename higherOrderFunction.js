64ba41b2ca98509aebb60247ad6bcf5c6e5c2d5f
Damilare Oyediran <43407276+ddoyediran@users.noreply.github.com>
2022-10-29 12:56:58 +0200

    Update higherOrderFunction.js

--- higherOrderFunction.js
+++ higherOrderFunction.js
@@ -1,1 +1,20 @@
 
+// Question 3
+/**
+  Write a function called isEven which takes in a number and returns true if the number is even and returns false if it is not
+*/
+
+function checkEven(num){
+  if(num % 2 === 0){
+    return true
+  }
+  
+  return false
+}
+
+function isEven(aNum, callback){
+  return callback(aNum);
+}
+
+console.log(isEven(2, checkEven)) // true
+console.log(isEven(3, checkEven)) // false
